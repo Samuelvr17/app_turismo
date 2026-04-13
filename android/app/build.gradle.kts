c3babe10ed8e8eff7372216e2ea77e5efaa5eb79
Samuel Vergara <samuelvergarar@gmail.com>
2026-04-13 08:23:55 -0500

    Guardar cambios pendientes antes de crear nueva rama

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,12 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("../../arcgis_maps_core/android")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
